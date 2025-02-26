65cc64b7e4a2f38dcd7ab02ccd472c076707738f
Arthurmedina07 <arthur12.medina@gmail.com>
2025-02-26 23:36:28 +0000

    commit

---
+++ DesafioSuperTrunfo.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main (){
+
+//cadastro da primeira carta do trunfo
+int populacao;
+int turismo;
+float area; 
+float pib;
+char estado [3] = 'A';
+char codigo [10] = 'A01';
+char cidade [50] = 'sao paulo';
+
+printf("estado: \n");
+scanf("%s", &estado);
+
+printf("codigo: \n");
+scanf("%s", &codigo);
+
+printf("nome da cidade: sao paulo \n");
+
+printf("populacao: 11.5 milhoes de habitantes \n");
+
+printf("area: 1.510 km \n");
+
+printf("PIB: 2.719.751 bilhoes \n");
+
+printf("numeros de pontos turisticos: 66 \n");
+//cadastro da segunda carta do trunfo
+
+
+int populacao1;
+int turismo1;
+float area1;
+float pib1;
+char estado1 [3] = 'B';
+char codigo1 [10] = 'B01';
+char cidade1 [50] = 'fortaleza';
+printf("estado: \n");
+scanf("%s", &estado1);
+printf("codigo: \n");
+scanf("%s", &codigo1);
+printf("nome da cidade: fortaleza \n");
+printf("populaçao: 2.520 milhoes de habitantes \n");
+printf("area: 313 km \n");
+printf("pib: 27.17 bilhoes ");
+printf("numeros de pontos turisticos: 79 \n"); 
+
+}
